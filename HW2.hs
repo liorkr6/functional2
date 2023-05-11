2fddfae0eec05705cacab78df32d6fc2ab1888fb
Lior Krinberg <lior.krinberg@runi.ac.il>
2023-05-11 17:12:32 +0300

    almost done

--- HW2.hs
+++ HW2.hs
@@ -21,48 +21,85 @@ import Prelude (Bool (..), Char, Either (..), Enum (..), Eq (..), Int, Maybe (..
 
 -- Section 1: String matching
 isPrefixOf :: String -> String -> Bool
+isPrefixOf str1 str2 = str1 == take (length str1) str2
+-- >>> "foo" `isPrefixOf` "foobar"
+-- >>> "foo" `isPrefixOf` "xfoobar"
+-- >>> "bard" `isPrefixOf` "foobar"
+-- True
+-- False
+-- False
+
+
 isInfixOf :: String -> String -> Bool
+isInfixOf _ [] = False
+isInfixOf infixStr (x : xs) = isPrefixOf infixStr (x:xs) || isInfixOf infixStr xs
+-- >>> "oob" `isInfixOf` "foobar"
+-- >>> "ooo" `isInfixOf` "foobar"
+-- True
+-- False
+
 isSuffixOf :: String -> String -> Bool
+isSuffixOf suffixStr str = reverse suffixStr `isPrefixOf` reverse str
+-- >>> "bar" `isSuffixOf` "foobar"
+-- True
+
 isSubseuenceOf :: String -> String -> Bool
+isSubseuenceOf _ [] = False
+isSubseuenceOf [] _ = True
+isSubseuenceOf (x:xs) (y:ys) = 
+    if x == y then isSubseuenceOf xs ys else isSubseuenceOf (x:xs) ys
 
+-- >>> "oa" `isSubseuenceOf` "foobar"
+-- >>> "ao" `isSubseuenceOf` "foobar"
+-- True
+-- False
 
--- Section 2: Document searching
+-- -- Section 2: Document searching
 type Phrase = String
 data Query = All [Query] | Any [Query] | None [Query] | Literal Phrase deriving Show
 type Document = String
+
+isDocumentMatch:: Query -> Document -> Bool
+isDocumentMatch (Literal phrase) d = isInfixOf phrase d
+isDocumentMatch (All quries) d = all (isDocumentMatch queries d)
+isDocumentMatch (Any quries) d = any (isDocumentMatch queries d)
+isDocumentMatch (None _) d = None (isDocumentMatch queries d)
+
 findDocuments :: Query -> [Document] -> ([Document], [Document])
 
 
--- Section 3: InfiniteList
-data InfiniteList a = a :> InfiniteList a
-infixr 3 :>
-
-itoList :: InfiniteList a -> [a]
-
-irepeat :: a -> InfiniteList a
-iiterate :: (a -> a) -> a -> InfiniteList a
-icycle :: [a] -> InfiniteList a
-
-naturals :: InfiniteList Int
-integers :: InfiniteList Int
-
-imap :: (a -> b) -> InfiniteList a -> InfiniteList b
-iscan :: (a -> b -> b) -> b -> InfiniteList a -> InfiniteList b
-
-izip :: InfiniteList a -> InfiniteList b -> InfiniteList (a, b)
-interleave :: InfiniteList a -> InfiniteList a -> InfiniteList a
-
-iinits :: InfiniteList a -> InfiniteList [a]
-itails :: InfiniteList a -> InfiniteList (InfiniteList a)
-
--- Bonus: if you don't wish to implement this, simply write ifind = undefined
-ifind :: forall a. (a -> Bool) -> InfiniteList (InfiniteList a) -> Bool
 
 
--- Section 4: Binary trees (no necessarily search trees)
-data Tree a = EmptyTree | Tree (Tree a) a (Tree a) deriving Show
-preOrder :: Tree a -> [a]
-postOrder :: Tree a -> [a]
-inOrder :: Tree a -> [a]
-levelOrder :: Tree a -> [a]
-fromListLevelOrder :: [a] -> Tree a
+-- -- Section 3: InfiniteList
+-- data InfiniteList a = a :> InfiniteList a
+-- infixr 3 :>
+
+-- itoList :: InfiniteList a -> [a]
+
+-- irepeat :: a -> InfiniteList a
+-- iiterate :: (a -> a) -> a -> InfiniteList a
+-- icycle :: [a] -> InfiniteList a
+
+-- naturals :: InfiniteList Int
+-- integers :: InfiniteList Int
+
+-- imap :: (a -> b) -> InfiniteList a -> InfiniteList b
+-- iscan :: (a -> b -> b) -> b -> InfiniteList a -> InfiniteList b
+
+-- izip :: InfiniteList a -> InfiniteList b -> InfiniteList (a, b)
+-- interleave :: InfiniteList a -> InfiniteList a -> InfiniteList a
+
+-- iinits :: InfiniteList a -> InfiniteList [a]
+-- itails :: InfiniteList a -> InfiniteList (InfiniteList a)
+
+-- -- Bonus: if you don't wish to implement this, simply write ifind = undefined
+-- ifind :: forall a. (a -> Bool) -> InfiniteList (InfiniteList a) -> Bool
+
+
+-- -- Section 4: Binary trees (no necessarily search trees)
+-- data Tree a = EmptyTree | Tree (Tree a) a (Tree a) deriving Show
+-- preOrder :: Tree a -> [a]
+-- postOrder :: Tree a -> [a]
+-- inOrder :: Tree a -> [a]
+-- levelOrder :: Tree a -> [a]
+-- fromListLevelOrder :: [a] -> Tree a
