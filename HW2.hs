d73db503141692280df32f39a0659eff9c9a17f6
Lior Krinberg <lior.krinberg@runi.ac.il>
2023-05-15 18:57:49 +0300

    almost almost done

--- HW2.hs
+++ HW2.hs
@@ -18,87 +18,92 @@ import Data.List (find, foldl', uncons)
 import Data.Maybe (catMaybes, fromMaybe, isJust, isNothing, listToMaybe, mapMaybe, maybe)
 import Prelude (Bool (..), Char, Either (..), Enum (..), Eq (..), Int, Maybe (..), Num (..), Ord (..), Show (..), String, all, and, any, concat, concatMap, const, curry, div, drop, dropWhile, elem, error, filter, flip, foldl, foldr, fst, id, init, last, length, lookup, map, maximum, minimum, mod, not, notElem, null, or, otherwise, product, reverse, snd, sum, tail, take, takeWhile, uncurry, undefined, unzip, zip, zipWith, (!!), ($), (&&), (++), (.), (||))
 
-
 -- Section 1: String matching
 isPrefixOf :: String -> String -> Bool
 isPrefixOf str1 str2 = str1 == take (length str1) str2
--- >>> "foo" `isPrefixOf` "foobar"
--- >>> "foo" `isPrefixOf` "xfoobar"
--- >>> "bard" `isPrefixOf` "foobar"
--- True
--- False
--- False
-
 
 isInfixOf :: String -> String -> Bool
 isInfixOf _ [] = False
 isInfixOf infixStr (x : xs) = isPrefixOf infixStr (x:xs) || isInfixOf infixStr xs
--- >>> "oob" `isInfixOf` "foobar"
--- >>> "ooo" `isInfixOf` "foobar"
--- True
--- False
 
 isSuffixOf :: String -> String -> Bool
 isSuffixOf suffixStr str = reverse suffixStr `isPrefixOf` reverse str
--- >>> "bar" `isSuffixOf` "foobar"
--- True
 
 isSubseuenceOf :: String -> String -> Bool
 isSubseuenceOf _ [] = False
 isSubseuenceOf [] _ = True
-isSubseuenceOf (x:xs) (y:ys) = 
+isSubseuenceOf (x:xs) (y:ys) =
     if x == y then isSubseuenceOf xs ys else isSubseuenceOf (x:xs) ys
-
--- >>> "oa" `isSubseuenceOf` "foobar"
--- >>> "ao" `isSubseuenceOf` "foobar"
--- True
--- False
-
 -- -- Section 2: Document searching
 type Phrase = String
 data Query = All [Query] | Any [Query] | None [Query] | Literal Phrase deriving Show
 type Document = String
 
-isDocumentMatch:: Query -> Document -> Bool
-isDocumentMatch (Literal phrase) d = isInfixOf phrase d
-isDocumentMatch (All quries) d = all (isDocumentMatch queries d)
-isDocumentMatch (Any quries) d = any (isDocumentMatch queries d)
-isDocumentMatch (None _) d = None (isDocumentMatch queries d)
+isDocumentMatch:: Document -> Query -> Bool
+isDocumentMatch d q = case q of
+  Literal phrase -> phrase `isInfixOf` d
+  All queries -> all (isDocumentMatch d) queries
+  Any queries -> any (isDocumentMatch d) queries
+  None queries -> not (any (isDocumentMatch d) queries)
 
 findDocuments :: Query -> [Document] -> ([Document], [Document])
+findDocuments query = foldr match ([], [])
+  where
+    match doc (matches, nonMatches)
+      | isDocumentMatch doc query  = (doc : matches, nonMatches)
+      | otherwise = (matches, doc : nonMatches)
 
+-- Section 3: InfiniteList
+data InfiniteList a = a :> InfiniteList a
+infixr 3 :>
 
+itoList :: InfiniteList a -> [a]
+itoList (x:>xs) = x : itoList xs
 
+irepeat :: a -> InfiniteList a
+irepeat x = x :> irepeat x
 
--- -- Section 3: InfiniteList
--- data InfiniteList a = a :> InfiniteList a
--- infixr 3 :>
+iiterate :: (a -> a) -> a -> InfiniteList a
+iiterate f x = x :> iiterate f (f x)
 
--- itoList :: InfiniteList a -> [a]
+icycle :: [a] -> InfiniteList a
+icycle list = foldr (:>) (icycle list) list
 
--- irepeat :: a -> InfiniteList a
--- iiterate :: (a -> a) -> a -> InfiniteList a
--- icycle :: [a] -> InfiniteList a
+naturals :: InfiniteList Int
+naturals = iiterate (+1) 0
 
--- naturals :: InfiniteList Int
--- integers :: InfiniteList Int
+sample :: InfiniteList a -> [a]
+sample = take 10 . itoList
 
--- imap :: (a -> b) -> InfiniteList a -> InfiniteList b
--- iscan :: (a -> b -> b) -> b -> InfiniteList a -> InfiniteList b
+integers :: InfiniteList Int
+integers = iiterate (\n -> if n > 0 then -n else -n+1) 0
 
--- izip :: InfiniteList a -> InfiniteList b -> InfiniteList (a, b)
--- interleave :: InfiniteList a -> InfiniteList a -> InfiniteList a
+imap :: (a -> b) -> InfiniteList a -> InfiniteList b
+imap f (x :> xs) =  f x :> imap f xs
 
--- iinits :: InfiniteList a -> InfiniteList [a]
--- itails :: InfiniteList a -> InfiniteList (InfiniteList a)
+iscan :: (a -> b -> b) -> b -> InfiniteList a -> InfiniteList b
+iscan f x (y :> ys) = f y x :> iscan f (f y x) ys
+
+izip :: InfiniteList a -> InfiniteList b -> InfiniteList (a, b)
+izip (x :> xs) (y :> ys) = (x, y) :> izip xs ys
+
+interleave :: InfiniteList a -> InfiniteList a -> InfiniteList a
+interleave (x:>xs) (y:>ys)= x :> y :> interleave xs ys
+
+iinits :: InfiniteList a -> InfiniteList [a]
+iinits xs = imap (\n -> take n (itoList xs)) naturals
+
+itails :: InfiniteList a -> InfiniteList (InfiniteList a)
+itails (x :> xs) = (x :> xs) :> itails xs
 
 -- -- Bonus: if you don't wish to implement this, simply write ifind = undefined
 -- ifind :: forall a. (a -> Bool) -> InfiniteList (InfiniteList a) -> Bool
 
-
 -- -- Section 4: Binary trees (no necessarily search trees)
--- data Tree a = EmptyTree | Tree (Tree a) a (Tree a) deriving Show
--- preOrder :: Tree a -> [a]
+data Tree a = EmptyTree | Tree (Tree a) a (Tree a) deriving Show
+preOrder :: Tree a -> [a]
+preOrder tree = case tree of
+    EmptyTree 
 -- postOrder :: Tree a -> [a]
 -- inOrder :: Tree a -> [a]
 -- levelOrder :: Tree a -> [a]
